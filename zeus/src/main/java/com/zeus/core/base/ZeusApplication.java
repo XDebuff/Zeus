42ab9971e1942aa5e102db2f4219138390242214
ZDebuff <458796489@qq.com>
2020-09-07 12:01:09 +0800

    无百度sdk

--- zeus/src/main/java/com/zeus/core/base/ZeusApplication.java
+++ zeus/src/main/java/com/zeus/core/base/ZeusApplication.java
@@ -5,8 +5,6 @@ package com.zeus.core.base;
 //import com.baidu.mapapi.CoordType;
 //import com.baidu.mapapi.SDKInitializer;
 
-import com.baidu.mapapi.CoordType;
-import com.baidu.mapapi.SDKInitializer;
 import com.facebook.stetho.Stetho;
 import com.zeus.core.cache.AppConfig;
 import com.zeus.core.cache.SPUtils;
@@ -27,9 +25,9 @@ public class ZeusApplication extends MultiDexApplication {
         SPUtils.init(this);
         AppConfig.init(getApplicationContext(), "zeus");
         //在使用SDK各组件之前初始化context信息，传入ApplicationContext
-        SDKInitializer.initialize(this);
+//        SDKInitializer.initialize(this);
         //自4.3.0起，百度地图SDK所有接口均支持百度坐标和国测局坐标，用此方法设置您使用的坐标类型.
         //包括BD09LL和GCJ02两种坐标，默认是BD09LL坐标。
-        SDKInitializer.setCoordType(CoordType.BD09LL);
+//        SDKInitializer.setCoordType(CoordType.BD09LL);
     }
 }
